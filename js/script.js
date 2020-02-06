a9730b67651691a14a73dab136a2ffa06d49d395
leejaebin <dmlwlsdk07@gmail.com>
2020-02-06 22:38:17 +0900

    제환이가 지울까봐 하는 커밋

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ function dropdown(){
         var optionbtn = document.getElementById("option-button");
         optionbtn.style.backgroundColor = "white";
         optionbtn.style.border = "1px solid white"
-        optionbtn.src = "../img/cross-out.png"
+        document.getElementById("option-button-img").src = "../img/cross-out.png"
         isdown = true;
     }
     else{
@@ -13,6 +13,29 @@ function dropdown(){
         var optionbtn = document.getElementById("option-button");
         optionbtn.style.backgroundColor = "#70408b";
         optionbtn.style.border = "1px solid #70408b"
+        document.getElementById("option-button-img").src = "../img/arrow.png"
         isdown = false;
     }
+}
+
+function sexClick(){
+    var optionContents = document.getElementsByClassName("option-contents-div")[0];
+    document.getElementsByClassName("sex-select")[0].style = "display: block; margin-left:26px;"
+    optionContents.style = "-webkit-filter: blur(5px); -moz-filter: blur(5px);-o-filter: blur(5px); -ms-filter: blur(5px); filter: blur(5px);background-color: #FFFFFF;"; // Code for Safari 3.1 to 6.0
+    optionContents.style.transition = "all 0.5s";
+}
+
+function cancelSex(){
+    document.getElementsByClassName("sex-select")[0].style = "display: none;";
+    var optionContents = document.getElementsByClassName("option-contents-div")[0];
+    optionContents.style = "";
+}
+
+function clickedMan(){
+    document.getElementById("sexValue").value = "man";
+    document.getElementById("sex-diamond-text").innerHTML = "<strong>남</strong>";
+}
+function clickedWoman(){
+    document.getElementById("sexValue").value = "woman";
+    document.getElementById("sex-diamond-text").innerHTML = "<strong>여</strong>";
 }
