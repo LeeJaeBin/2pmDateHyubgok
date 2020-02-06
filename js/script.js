787745f2124e18c2728975a632d9b575b6231c41
LeeJaeBin <dmlwlsdk07@gmail.com>
2020-02-06 19:01:13 +0900

    commit yeah

--- js/script.js
+++ js/script.js
@@ -2,10 +2,17 @@ var isdown = false;
 function dropdown(){
     if(!isdown){
         document.getElementsByClassName("option-select-div")[0].style.display = "block";
+        var optionbtn = document.getElementById("option-button");
+        optionbtn.style.backgroundColor = "white";
+        optionbtn.style.border = "1px solid white"
+        optionbtn.src = "../img/cross-out.png"
         isdown = true;
     }
     else{
         document.getElementsByClassName("option-select-div")[0].style.display = "none";
+        var optionbtn = document.getElementById("option-button");
+        optionbtn.style.backgroundColor = "#70408b";
+        optionbtn.style.border = "1px solid #70408b"
         isdown = false;
     }
 }
